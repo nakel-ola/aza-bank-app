62143a1c146b078e4ad3fd4153aee61acc133c56
Olamilekan <nunuolamilekan@gmail.com>
2022-12-25 15:21:23 +0100

    upload fix (8)

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,18 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 // import * as graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.js";
+import { graphqlUploadExpress } from "graphql-upload-minimal";
 import { AppModule } from "./app.module";
-// import { graphqlUploadExpress } from "graphql-upload-minimal";
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(graphqlUploadExpress({ maxFileSize: 2 * 1024 * 1024, maxFiles: 1 }));
+  app.use(
+    graphqlUploadExpress({
+      maxFileSize: 2 * 1024 * 1024,
+      maxFiles: 1,
+      environment: "lambda",
+    })
+  );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({ allowedHeaders: "*", origin: "*" });
   await app.listen(process.env.PORT || 3333, "0.0.0.0", async () => {
@@ -15,4 +20,4 @@ async function bootstrap() {
   });
 }
 
-bootstrap()
+bootstrap();
