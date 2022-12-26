92e62a6ea69a7b75e2148c130b5460524cff6076
Olamilekan <nunuolamilekan@gmail.com>
2022-12-26 14:34:04 +0100

    fix server build

--- server-apollo/src/config.ts
+++ server-apollo/src/config.ts
@@ -8,9 +8,6 @@ const config = {
   mongodb_uri: process.env.MONGODB_URI,
   stmp_email: process.env.STMP_EMAIL,
   stmp_password: process.env.STMP_PASSWORD,
-  storage_bucket_name: process.env.STORAGE_BUCKET_NAME,
-  storage_credentials_path: process.env.STORAGE_CREDENTIALS_PATH,
-  storage_project_id: process.env.STORAGE_PROJECT_ID,
   email_from: '"Aza Bank Team" noreply@azabank.com',
 };
 export default config;
