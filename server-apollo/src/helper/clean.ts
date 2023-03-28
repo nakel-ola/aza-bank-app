36efcc0cc1342fce2ccba58644df289471f0a68e
Olamilekan <109567025+olamilekan21@users.noreply.github.com>
2023-03-28 11:42:12 +0100

    Delete server-apollo directory

--- server-apollo/src/helper/clean.ts
+++
@@ -1,9 +0,0 @@
-function clean(obj: any) {
-  for (var propName in obj) {
-    if (obj[propName] === null || obj[propName] === undefined) {
-      delete obj[propName];
-    }
-  }
-  return obj;
-}
-export default clean;
--- server-apollo/src/models/index.ts
+++
@@ -1,9 +0,0 @@
-import userModel from "./user.model";
-import validateModel from "./validate.model";
-import transactionModel from "./transaction.model";
-
-export default {
-  userModel,
-  validateModel,
-  transactionModel
-};
